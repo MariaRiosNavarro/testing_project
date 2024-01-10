d989a619c26bfabb34fa1b53fd8f1e0e6c962f0a
SuperCoder <propscode254@gmail.com>
2024-01-10 15:22:43 +0100

    get & post tests

--- src/horses/horses.controller.js
+++ src/horses/horses.controller.js
@@ -1,22 +1,30 @@
 import { Horse } from "./horses.model.js";
 
 export const postHorse = async (req, res) => {
-  const horse = new Horse(req.body);
   try {
+    const horse = new Horse(req.body);
     await horse.save();
-    res.status(201).end();
+    res.status(201).json(horse);
   } catch (error) {
     res.status(500).end();
   }
 };
 
 export const getHorses = async (req, res) => {
-  const horses = await Horse.find();
-  res.json(horses);
+  try {
+    const horses = await Horse.find();
+    res.status(200).json(horses);
+  } catch (error) {
+    res.status(500).end();
+  }
 };
 
 export const deleteHorse = async (req, res) => {
   const { _id } = req.body;
-  await Horse.findByIdAndDelete({ _id: _id });
-  res.send();
+  try {
+    await Horse.findByIdAndDelete({ _id: _id });
+    res.status(204).end();
+  } catch (error) {
+    res.status(500).end();
+  }
 };
--- tests/integration/horses.integration.test.js
+++ tests/integration/horses.integration.test.js
@@ -1,10 +1,60 @@
 import supertest from "supertest";
 import { app } from "../../src/app.js";
+import { Horse } from "../../src//horses/horses.model.js";
+import { jest } from "@jest/globals";
 
 const request = supertest(app);
 
-test("Ich erwarte ein Array wenn ich Get benutze", async () => {
-  const response = await request.get("/api/horses");
-  expect(response.statusCode).toBe(200);
-  expect(response.body).toBeInstanceOf(Array);
+describe('Check the Route "/" Get', () => {
+  test("I expect a code 200 when I use the route Get properly", async () => {
+    const response = await request.get("/api/horses");
+    expect(response.statusCode).toBe(200);
+  });
+
+  test("I expect an array of data when I use the ruote Get", async () => {
+    const response = await request.get("/api/horses");
+    expect(response.body).toBeInstanceOf(Array);
+  });
+  test("I expect a code 500 if I use the Get route incorrectly", async () => {
+    // Mock find function to throw an exception
+    jest.spyOn(Horse, "find").mockImplementationOnce(() => {
+      // find is a static method of the Horse class, not an instance method. So you have to spy on the find method directly in the Horse class.
+      // DONT USE Horse.prototyp like in POST
+      throw new Error("Simulated error");
+    });
+    const response = await request.get("/api/horses");
+    expect(response.statusCode).toBe(500);
+    // Restore the original implementation of the find function
+    jest.spyOn(Horse, "find").mockRestore();
+  });
+});
+
+// --------------------
+
+describe('Check the Route "/" POST', () => {
+  test("I expect a code 201 when I use the route Post properly", async () => {
+    const response = await request.post("/api/horses");
+    expect(response.statusCode).toBe(201);
+  });
+
+  test("I expect an object as response when I use the Ruote post", async () => {
+    const response = await request.post("/api/horses");
+    expect(response.body).toBeInstanceOf(Object);
+  });
+
+  test("I expect a code 500 if I use the post route incorrectly", async () => {
+    // Mock find function to throw an exception
+    jest.spyOn(Horse.prototype, "save").mockImplementationOnce(() => {
+      // USE Horse.prototyp for "save" in POST
+      throw new Error("Simulated error");
+    });
+    const response = await request.post("/api/horses").send({
+      name: "testhorse",
+      age: 2,
+      breed: "testbreed",
+    });
+    expect(response.statusCode).toBe(500);
+    // Restore the original implementation of the find function
+    jest.spyOn(Horse.prototype, "save").mockRestore();
+  });
 });
